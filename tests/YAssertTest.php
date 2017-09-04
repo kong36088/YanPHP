4cec00ab5b01e64472d4e6c045b10bd43fee614d
JIANGWEILONG <william@jwlchina.cn>
2017-09-04 15:09:20 +0800

    new test case

--- tests/YAssertTest.php
+++ tests/YAssertTest.php
@@ -8,14 +8,35 @@
 namespace TestNamespace;
 
 use PHPUnit\Framework\TestCase;
+use Yan\Core\Exception\YAssertionFailedException;
 use Yan\Core\YAssert;
 
 class YAssertTest extends TestCase
 {
+    public function testTrue()
+    {
+        $this->assertTrue(YAssert::true(1 === 1));
+    }
+
+    public function testAllTrue()
+    {
+        $this->assertTrue(YAssert::allTrue([true, 'a' => true]));
+        $this->assertTrue(YAssert::allTrue(true));
+    }
+
+    public function testEq(){
+        $this->assertTrue(YAssert::eq(1,'1'));
+    }
+
+    public function testNull(){
+        $this->assertTrue(YAssert::null(null));
+    }
+
     /**
-     * @Expect
+     * @expectedException YAssertionFailedException
      */
-    public function testAssertTrue(){
-        YAssert::allTue(1==2);
+    public function testException()
+    {
+        $this->assertTrue(YAssert::allTrue(1 == 2));
     }
 }
