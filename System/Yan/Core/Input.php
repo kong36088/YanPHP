b76559fbad6b8dee8cbd1f01690a4d747a1d505c
JIANGWEILONGGGG <william@jwlchina.cn>
2017-10-09 13:21:52 +0800

    optimize

--- System/Yan/Core/Input.php
+++ System/Yan/Core/Input.php
@@ -47,8 +47,8 @@ class Input
 
     public static function get($key = '')
     {
-        if (empty($key)) return self::_clean(self::$data);
-        return isset(self::$data[$key]) ? self::_clean(self::$data[$key]) : null;
+        if (empty($key)) return self::$data;
+        return isset(self::$data[$key]) ? self::$data[$key] : null;
     }
 
     public static function set($key,$value)
@@ -56,52 +56,4 @@ class Input
         self::$data[$key] = $value;
     }
 
-    /*
-    public static function post($key = '')
-    {
-        if (empty($key)) return self::_clean($_POST);
-        return isset($_POST[$key]) ? self::_clean($_POST[$key]) : null;
-    }
-
-    public static function put($key = '')
-    {
-        if (self::$method === 'PUT') {
-            return self::getRaw($key);
-        } else {
-            return null;
-        }
-    }
-
-    public static function delete($key = '')
-    {
-        if (self::$method === 'DELETE') {
-            return self::getRaw($key);
-        } else {
-            return null;
-        }
-    }
-
-    public static function input($key)
-    {
-        return self::getRaw($key);
-    }
-    */
-
-    protected static function _clean($data)
-    {
-        if (is_array($data)) {
-            foreach ($data as $k => $v) {
-                $data[$k] = self::_clean($v);
-            }
-            return $data;
-        }
-
-        return trim($data);
-    }
-
-    protected static function getRaw($key)
-    {
-        if (empty($key)) return self::_clean(self::$data);
-        return isset(self::$data[$key]) ? self::_clean(self::$data[$key]) : null;
-    }
 }
