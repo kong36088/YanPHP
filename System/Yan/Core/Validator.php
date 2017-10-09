6351e1d45033595cf7f7d24ce4d0d024f9a7842c
JIANGWEILONGGGG <william@jwlchina.cn>
2017-10-09 17:07:45 +0800

    新增入参规则支持

--- System/Yan/Core/Validator.php
+++ System/Yan/Core/Validator.php
@@ -16,13 +16,14 @@ class Validator
     /** @var \Respect\Validation\Validator */
     protected static $validator = null;
 
+    /** @var array 规则列表 */
     protected static $rules;
 
     public static function initialize()
     {
-        //TODO optional
         self::$rules = array(
             'required' => 'notOptional',
+            'optional' => 'Optional',
             'integer' => 'intType',
             'numeric' => 'numeric',
             'float' => 'floatType',
@@ -32,10 +33,9 @@ class Validator
             'json' => 'json',
             'email' => 'email',
             'domain' => 'domain',
-            //'file' => 'file',
 
             // params
-            //'in_list' => v::contains([1,2]),
+            'contain' => 'contains',
             'starts_with' => 'startsWith',
             'ends_with' => 'endsWith',
             'between' => 'Between',
@@ -44,6 +44,7 @@ class Validator
             'length' => 'length',
             'equal' => 'Equals',
             'regex' => 'regex',
+            'in' => 'In'
         );
 
     }
@@ -60,13 +61,25 @@ class Validator
     public static function validate($paramName, $input, string $rules, &$resultMsg = null): bool
     {
         $rulesArr = explode('|', $rules);
+
+        $optional = false;
+        if (($search = array_search('optional', $rulesArr)) !== false) {
+            $optional = true;
+            unset($rulesArr[$search]);
+        }
+
+        if (empty($rulesArr) || empty($rulesArr[0])) {
+            return true;
+        }
+
         //遍历所有用户定义的规则
+        $validate = v::class;
         foreach ($rulesArr as $r) {
             if (empty($r)) {
                 continue;
             }
 
-            preg_match('/([a-zA-Z_]*)(\[(.*)\]){0,1}/', $r, $matches);
+            preg_match('/([a-zA-Z_]*)(\((.*)\)){0,1}/', $r, $matches);
             $rule = $matches[1];
             //规则是否存在
             if (!array_key_exists($rule, self::$rules)) {
@@ -75,7 +88,12 @@ class Validator
             }
             $ruleParams = array();
             if (isset($matches[3])) {
-                $ruleParams = explode(',', $matches[3]);
+                if (strpos($matches[3], '[') === 0) {
+                    $matches[3] = substr($matches[3], 1, strlen($matches[3]) - 2);
+                    $ruleParams = [explode(',', $matches[3])];
+                } else {
+                    $ruleParams = explode(',', $matches[3]);
+                }
             }
             //利用反射获取rule所需入参个数
             $respectRule = "\\Respect\\Validation\\Rules\\" . self::$rules[$rule];
@@ -88,14 +106,20 @@ class Validator
 
             $ruleParams = array_slice($ruleParams, 0, $respectRuleParamNum);
 
-            $validate = call_user_func_array([v::class, self::$rules[$rule]], $ruleParams);
-            try {
-                $validate->setName($paramName)->assert($input);
-            } catch (NestedValidationException $exception) {
-                $resultMsg = $exception->getFullMessage();
-                return false;
-            }
+            /** @var \Respect\Validation\Validator $validate */
+            $validate = call_user_func_array([$validate, self::$rules[$rule]], $ruleParams);
+
         }
+        try {
+            if ($optional) {
+                $validate = v::optional($validate);
+            }
+            $validate->setName($paramName)->assert($input);
+        } catch (NestedValidationException $exception) {
+            $resultMsg = $exception->getFullMessage();
+            return false;
+        }
+
         return true;
     }
 }
