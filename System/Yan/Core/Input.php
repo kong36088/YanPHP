95a30df952f04e0a5dac873bedfa0d94e7466849
JIANGWEILONGGGG <william@jwlchina.cn>
2017-10-09 14:37:13 +0800

    Input module optimization

--- System/Yan/Core/Input.php
+++ System/Yan/Core/Input.php
@@ -11,7 +11,6 @@ use Yan\Core\Exception\FileNotExistException;
 use Yan\Core\Exception\InvalidArgumentException;
 use Yan\Core\Exception\RuntimeException;
 
-//TODO 优化
 class Input
 {
     protected static $data = array();
@@ -22,27 +21,11 @@ class Input
     {
         self::$method = strtoupper($_SERVER['REQUEST_METHOD']);
         parse_str(file_get_contents('php://input'), $input);
-        $input = array_merge($input, $_GET);
-        $input = array_merge($input, $_POST);
+        $input = array_merge($input, $_REQUEST);
+        $input = array_merge($input, $_FILES);
 
-        //根据Param/xxx.ini中配置的入参进行筛选
-        $paramFile = BASE_PATH . '/Param/' . Dispatcher::$controllerShortName . '.ini';
-        if (!file_exists($paramFile)) {
-            throwErr("file {$paramFile} does not exist", ReturnCode::SYSTEM_ERROR, FileNotExistException::class);
-        }
-        $paramRules = parse_ini_file($paramFile, true);
-        if (!$paramRules) {
-            throwErr("can not parse file {$paramFile}", ReturnCode::SYSTEM_ERROR, RuntimeException::class);
-        }
-        //规则验证
-        foreach ($paramRules[Dispatcher::$method] ?: [] as $key => $rule) {
-            $value = $input[$key] ?? null;
-            $ret = Validator::validate($key, $value, $rule, $msg);
-            if (!$ret) {
-                throwErr($msg, ReturnCode::INVALID_ARGUMENT, InvalidArgumentException::class);
-            }
-            self::$data[$key] = $input[$key];
-        }
+        self::$data = $input;
+
     }
 
     public static function get($key = '')
@@ -51,9 +34,21 @@ class Input
         return isset(self::$data[$key]) ? self::$data[$key] : null;
     }
 
-    public static function set($key,$value)
+    public static function set($key, $value)
     {
         self::$data[$key] = $value;
+    }
+
+    public static function setData(array $data)
+    {
+        self::$data = $data;
+    }
+
+    public static function batchSet(array $d)
+    {
+        foreach ($d as $key => $value) {
+            self::$data[$key] = $value;
+        }
     }
 
 }
--- System/yan.php
+++ System/yan.php
@@ -11,10 +11,20 @@ require_once $dirname."/../vendor/autoload.php";
 
 require_once $dirname."/Yan/Common/Functions.php";
 
+use Yan\Core\Config;
+use Yan\Core\Exception\FileNotExistException;
+use Yan\Core\Exception\RuntimeException;
+use Yan\Core\Log;
+use Yan\Core\Session;
+use Yan\Core\Database;
+use Yan\Core\Dispatcher;
+use Yan\Core\Validator;
+use Yan\Core\Input;
+use Yan\Core\ReturnCode;
 
-Yan\Core\Config::initialize();
+Config::initialize();
 
-Yan\Core\Log::initialize();
+Log::initialize();
 
 set_exception_handler('exceptionHandler');
 set_error_handler('errorHandler');
@@ -22,35 +32,57 @@ set_error_handler('errorHandler');
 /**
  * Session
  */
-\Yan\Core\Session::initialize();
+Session::initialize();
 
 /**
  * database
  */
-\Yan\Core\Database::initialize();
+Database::initialize();
 
 
 /**
  * router
  */
-\Yan\Core\Dispatcher::initialize();
+Dispatcher::initialize();
 $dispatch = \Yan\Core\Dispatcher::dispatch();
 
 /**
  * Validator
  */
-\Yan\Core\Validator::initialize();
+Validator::initialize();
 
 /**
  * Input
  */
-\Yan\Core\Input::initialize();
+Input::initialize();
+
+//根据Param/xxx.ini中配置的入参进行筛选
+$paramFile = BASE_PATH . '/Param/' . Dispatcher::$controllerShortName . '.ini';
+if (!file_exists($paramFile)) {
+    throwErr("file {$paramFile} does not exist", ReturnCode::SYSTEM_ERROR, FileNotExistException::class);
+}
+$paramRules = parse_ini_file($paramFile, true);
+if (!$paramRules) {
+    throwErr("can not parse file {$paramFile}", ReturnCode::SYSTEM_ERROR, RuntimeException::class);
+}
+//规则验证
+$input = array();
+foreach ($paramRules[Dispatcher::$method] ?: [] as $key => $rule) {
+    $value = Input::get($key) ?? null;
+    $ret = Validator::validate($key, $value, $rule, $msg);
+    if (!$ret) {
+        throwErr($msg, ReturnCode::INVALID_ARGUMENT, InvalidArgumentException::class);
+    }
+    $input[$key] = $value;
+}
+//覆盖Input中data值，只设置配置文件中配置的key
+Input::setData($input);
 
 $controller = new $dispatch[0];
 $result = call_user_func([$controller, $dispatch[1]]);
 
 if (!$result instanceof \Yan\Core\Compo\ResultInterface) {
-    throwErr('result is not instance of ResultInterface',\Yan\Core\ReturnCode::SYSTEM_ERROR,RuntimeException::class);
+    throwErr('result is not instance of ResultInterface',ReturnCode::SYSTEM_ERROR,RuntimeException::class);
 }
 
 showResult($result);
